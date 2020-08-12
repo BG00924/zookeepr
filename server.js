f2b7019d0bddb19a3ab0a70a6dbfd17bd1274a6a
BG00924 <BG00924@users.noreply.github.com>
2020-08-12 15:40:17 -0500

    added post request capability

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+// needed to write to json
+const fs = require('fs')
+const path = require('path')
 // allows the app to use to vary ports
 const PORT = process.env.PORT || 3001
 // instantiates the server
 const app = express()
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}))
+// parse incoming json data
+app.use(express.json())
 // requires the animal data
 const { animals } = require('./data/animals')
 // filter code
@@ -48,6 +55,36 @@ function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0]
     return result
 }
+// creates a new animal from data received from the post request
+function createNewAnimal(body, animalsArray) {
+    // console.log(body)
+    // our functions main code will go here
+    const animal = body
+    animalsArray.push (animal)
+    // writes to animal.json
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    )
+    // return finished code to post route for response
+    return animal;
+}
+// validates data received from user prior to writing to json
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
 // adds the route
 app.get('/api/animals', (req, res) => {
     let results = animals
@@ -63,6 +100,23 @@ app.get('/api/animals/:id', (req, res) => {
         res.json(result)
     } else {
         res.send(404)
+    }
+})
+// allows user to populate data to the server
+app.post('/api/animals', (req, res) => {
+    // req.body is where out incoming content will be
+    // console.log(req.body)
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString()
+    // add animal to json file and animals array in this function
+    // const animal = createNewAnimal(req.body, animals)
+    // res.json(req.body)
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.')
+    } else {
+        const animal = createNewAnimal(req.body, animals)
+        res.json(animal)
     }
 })
 // makes the server listen
